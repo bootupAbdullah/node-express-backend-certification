0e2c5efe2ecd8558b2d2ce60662fbc65ec2a91e2
Abdulla Durrani <Abdullahkd@gmail.com>
2025-02-20 12:19:14 -0500

    updated myApp.js with most recent code

--- myApp.js
+++ myApp.js
@@ -1,37 +1,47 @@
 let express = require('express');
-require('dotenv').config()
 let app = express();
+require('dotenv').config()
 
-console.log("Hello Wold");
+
+console.log("Hello World");
 
 let absolutePath = __dirname + '/views/index.html';
-let publicDir   = __dirname + '/public';
+let publicDir = __dirname + '/public';
 
-// app.get("/",function(req, res) {
-// res.send('Hello Express')
+// app.get("/", function(req, res) {
+//     res.send("Hello Express")
 // })
+
+app.use('/public', express.static(publicDir));
+
+app.use("/", function(req, res, next) {
+    console.log(req.method +  " " + req.path + " - " + req.ip)
+    next();
+});
 
 
 app.get("/", function(req, res) {
     res.sendFile(absolutePath)
+});
+
+app.get("/", function(req, res, next) {
+    console.log(req.method + req.path + req.ip)
+    next();
 })
-
-app.use("/public", express.static(publicDir));
-
-// app.get("/json", function(req, res) {
-//     res.json({"message" : "Hello json"})
-// })
-
 
 app.get("/json", function(req, res) {
 
     if (process.env.MESSAGE_STYLE === "uppercase") {
-        res.json({"message" : "HELLO JSON"})
+        res.json({
+            "message" : "HELLO JSON"
+        })
     } else {
-        res.json({"message" : "Hello json"})
+        res.json({
+            "message" : "Hello json"
+        })
     }
-})
 
+});
 
 
 
